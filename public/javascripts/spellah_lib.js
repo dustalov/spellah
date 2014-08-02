4be88322e7147b82d29d289b208a82688031851b
Igor Lashkov <lashkov@lashkov-osx2.local>
2014-08-02 23:30:35 +0400

    Added ability to watch jb for each block;

--- public/javascripts/spellah_lib.js
+++ public/javascripts/spellah_lib.js
@@ -27,7 +27,7 @@ function getNGrams(){
     var arr = str.split(/[ ,]+/).filter(Boolean);
 
     if(arr.length <= ngrams){
-        return [str];
+        return {0:str};
     }
 
     //alert(arr.toString());
@@ -47,15 +47,28 @@ function getNGrams(){
         }
 
         id++;
-        data.push({ label: id, value: str });
+        //data.push({ label: id, value: str });
+        data.push(str);
         str = "";
     }
+
+    //alert(arr.toString());
+
+    return data;
+
+
+    //очищаем поле ответа
+    /*document.getElementById("response").innerHTML = "";
 
     for (var key in data) {
       if (key === 'length' || !data.hasOwnProperty(key)) continue;
       var value = data[key];
-      alert(value.value);
-    }
+
+
+      httpGet("/check?q=" + value.value, value.value);
+
+      //alert(value.value);
+    }*/
 
     //alert(data.toString());
 }
@@ -70,12 +83,39 @@ function init(){
     var myEl = document.getElementById('getit');
 
     myEl.addEventListener('click', function() {
-        var arr = getNGrams();
-        alert(arr.toString());
+    
+    var arr = getNGrams();
+
+    document.getElementById("response").innerHTML = "";
+
+    for (var key in arr) {
+
+      //alert(arr[key]);
+
+      httpGet("/check?q=" + arr[key], arr[key]);
+
+      //alert(value.value);
+    }
+
+        //alert(arr.toString());
     }, false);
 
-
-
-
     highlightWords(getWords());
+}
+
+function httpGet(theUrl, ngramBlock){
+    //document.write(theUrl);
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.onreadystatechange = function(){handleReadyStateChange(ngramBlock)};
+    xmlHttp.send(null);
+
+    function handleReadyStateChange(block) {
+      if (xmlHttp.readyState == 4) {
+        if (xmlHttp.status == 200) {
+          document.getElementById("response").innerHTML += block + " " + xmlHttp.responseText + "</br >";
+        }
+      }
+    }
 }
