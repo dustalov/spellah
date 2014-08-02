ebd007cd5931ef3673f0946cf832596a07eacccb
Dmitry Ustalov <dmitry@eveel.ru>
2014-08-02 21:13:44 +0400

    The first working something

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var endpoint = 'http://weblm.research.microsoft.com/rest.svc/bing-body/2013-12/5/jp';
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Spellah' });
+});
+
+router.get('/check', function(req, res) {
+  query = { u: process.env.MICROSOFT_NGRAMS_KEY, p: req.query.q, format: 'json' }
+  request.get({ url: endpoint, qs: query }, function (e, r, value) {
+    res.format({
+      json: function() {
+        res.send({ jp: parseFloat(value) });
+      }
+    });
+  })
 });
 
 module.exports = router;
