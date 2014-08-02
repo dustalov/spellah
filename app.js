51e58b9b765cd666011426e474f38359c5828ead
Dmitry Ustalov <dmitry@eveel.ru>
2014-08-02 16:40:22 +0400

    Use ejs-locals

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+var engine = require('ejs-locals');
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
