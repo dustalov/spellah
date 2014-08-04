45870213bac1aa4860ddf5f2d8bb2553d02a2b97
Igor Lashkov <lashkov@lashkov-osx2.local>
2014-08-04 22:32:25 +0400

    Added textarea highlight feature many improvements new UI

--- public/javascripts/spellah_lib.js
+++ public/javascripts/spellah_lib.js
@@ -1,53 +1,108 @@
 
+function highlightWords(wordsArr, totalCheck){
 
-function getWords() {
-    return ['Lorem ipsum'];
-}
+    var inputText = jQuery("textarea#demo").val();
 
-function highlightWords(arr){
+    var corrected = [];
+    var partly = [];
+    var wrong = [];
 
-    $('textarea').highlightTextarea({
-    words: arr
+
+    for(var i = 0; i < totalCheck.length; i++){
+        var ar = getIndicesOf(wordsArr[i], inputText, false);
+
+
+        for(var j = 0; j < ar.length; j++){
+
+            var arr = [ar[j], ar[j] + wordsArr[i].length];
+
+            if(totalCheck[i] == 0){
+                corrected.push(arr);
+            } else if(totalCheck[i] == 1){
+                partly.push(arr);
+            } else {
+                wrong.push(arr);
+            }
+        }
+    }
+
+
+    $('textarea#demo').highlightTextarea({
+    ranges: 
+
+        [{
+            color: '#00FF00',
+            ranges: corrected
+          }, {
+            color: '#FFFF00',
+            ranges: partly
+          }, {
+            color: '#FF0000',
+            ranges: wrong
+          }]
   });
 
 }
 
-function getNGrams(){
+function getIndicesOf(searchStr, str, caseSensitive) {
+    var startIndex = 0, searchStrLen = searchStr.length;
+    var index, indices = [];
+    if (!caseSensitive) {
+        str = str.toLowerCase();
+        searchStr = searchStr.toLowerCase();
+    }
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+        indices.push(index);
+        startIndex = index + searchStrLen;
+    }
+    return indices;
+}
+
+
+function getWords(text){
+    return text.split(/[ ,]+/).filter(Boolean);
+}
+
+
+function getNGrams(inputText){
 
     var ngrams = 4;
 
-    var str = jQuery("textarea#demo").val();
 
-    if(!str){
+    if(!inputText){
         alert('Value is empty');
         return;
     }
+
+    var wordsSequence = getWords(inputText);
+
     //alert(thought);
 
-    var arr = str.split(/[ ,]+/).filter(Boolean);
 
-    if(arr.length <= ngrams){
-        return {0:str};
+    if(wordsSequence.length <= ngrams){
+        return {0:inputText};
     }
 
-    //alert(arr.toString());
+    //alert(wordsSequence.length);
+
     var data = [];
     var id = 0;
     
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < wordsSequence.length; i++) {
 
-        if(i + ngrams > arr.length){
+        if(i + ngrams > wordsSequence.length){
             break;
         }
 
         var str = "";
         for(var j = 0; j < ngrams; j++){
             str = str + " ";
-            str = str.concat(arr[i + j]);
+            str = str.concat(wordsSequence[i + j]);
         }
 
         id++;
         //data.push({ label: id, value: str });
+        //alert(str);
         data.push(str);
         str = "";
     }
@@ -55,52 +110,115 @@ function getNGrams(){
     //alert(arr.toString());
 
     return data;
-
-
-    //очищаем поле ответа
-    /*document.getElementById("response").innerHTML = "";
-
-    for (var key in data) {
-      if (key === 'length' || !data.hasOwnProperty(key)) continue;
-      var value = data[key];
-
-
-      httpGet("/check?q=" + value.value, value.value);
-
-      //alert(value.value);
-    }*/
-
-    //alert(data.toString());
 }
 
 function init(){
+
+
+    document.getElementById("demo").focus();
+
+    $(".yt-button").click(function(){
+         $(this).toggleClass("yt-state_active");
+    });
+
+
+
     
     $('#myform').submit(function() {
       return false;
+    });
+
+    $('textarea#demo').bind('input propertychange', function() {
+
+        $('.container').remove();
+        $('.highlightTextarea').contents().unwrap();
+
+        document.getElementById("demo").focus();
+
     });
 
 
     var myEl = document.getElementById('getit');
 
     myEl.addEventListener('click', function() {
+
+
+    var inputText = jQuery("textarea#demo").val();
     
-    var arr = getNGrams();
+
+    var ngramsArr = getNGrams(inputText);
 
     document.getElementById("response").innerHTML = "";
 
-    for (var key in arr) {
 
-      //alert(arr[key]);
+    var data = []; //check array
+    var ajaxReqs = [];
+    for (var key in ngramsArr) {
 
-      httpGet("/check?q=" + arr[key], arr[key]);
-
-      //alert(value.value);
+        ajaxReqs.push($.ajax({
+            url:  ("/check?q=" + ngramsArr[key].trim()).trim(),
+            type: 'GET',
+            timeout: 30000,
+            error: function(){
+                return true;
+            },
+            success: function(msg){ 
+                data.push(msg.check);
+                document.getElementById("response").innerHTML += JSON.stringify(msg) + "</br >";
+            return true
+            },
+          cache: true
+            /* AJAX settings */
+        }));
     }
+    $.when.apply($, ajaxReqs).then(function() {
+        // all requests are complete
 
-        //alert(arr.toString());
+        var words = getWords(inputText);
+
+
+        var totalCheck = Array.apply(null, new Array(words.length)).map(Number.prototype.valueOf,0);
+
+
+        var step = 1; 
+        var j = 0;
+
+        for(var key in data){
+
+            var check = data[key];
+            
+            for(var i = 0; i < check.length; i++){
+                if((i + j) < totalCheck.length && check[i] > totalCheck[i + j]){
+                    totalCheck[i + j] = check[i];
+                }
+            }
+            j += step;
+
+            //alert(totalCheck.toString());
+        }
+
+
+        highlightWords(words, totalCheck);
+    });
+
+
+
+    }, false); //click event
+
+
+    var myEl2 = document.getElementById('clearBtn');
+
+    myEl2.addEventListener('click', function() {
+
+        $('.container').remove();
+        $('.highlightTextarea').contents().unwrap();
+        
+        var area = document.getElementById('demo');
+        area.value='';
+        area.focus();
     }, false);
 
-    highlightWords(getWords());
+    
 }
 
 function httpGet(theUrl, ngramBlock){
