d5bf138bba0b6c22fbbf200396e736e45fa5a8a1
Dmitry Ustalov <dmitry@eveel.ru>
2014-08-05 14:40:41 +0400

    Add the ID parameter into the response

--- routes/index.js
+++ routes/index.js
@@ -59,10 +59,10 @@ function check(tokens, ngrams) {
   return levels
 }
 
-function reply(res, tokens, ngrams) {
+function reply(req, res, tokens, ngrams) {
   res.format({
     json: function() {
-      res.send({ check: check(tokens, ngrams), ngrams: ngrams });
+      res.send({ id: req.query.id, check: check(tokens, ngrams), ngrams: ngrams });
     }
   });
 }
@@ -85,12 +85,12 @@ router.get('/check', function(req, res) {
       request.post({ url: endpoint, qs: query, body: body.join("\n") }, function (e, r, value) {
         var ngrams = mapOf(body, JSON.parse(value))
         cache.set(key, value)
-        reply(res, tokens, ngrams)
+        reply(req, res, tokens, ngrams)
       })
     } else {
       console.log('cache hit "' + req.query.q + '" => ' + cached)
       var ngrams = mapOf(body, JSON.parse(cached))
-      reply(res, tokens, ngrams)
+      reply(req, res, tokens, ngrams)
     }
   });
 });
