ebc583fff2784493f02f72605ae32554e661cd57
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2019-03-07 15:57:45 +0100

    Clean up

--- app.js
+++ app.js
@@ -1,59 +1,66 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+#!/usr/bin/env node
 
-var routes = require('./routes/index');
+var express = require('express')
+var path = require('path')
+var favicon = require('static-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
-var app = express();
+var routes = require('./routes/index')
+
+var app = express()
 
 // view engine setup
-var engine = require('ejs-locals');
-app.engine('ejs', engine);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var engine = require('ejs-locals')
+app.engine('ejs', engine)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
+app.use('/', routes)
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
 
+var debug = require('debug')('spellah')
 
-module.exports = app;
+var port = process.env.PORT || 3000
+
+app.listen(port, function () {
+  debug('Spellah listening on port ' + port)
+})
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,39 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var request = require('request');
-var endpoint = 'https://westus.api.cognitive.microsoft.com/text/weblm/v1.0/calculateJointProbability?model=body';
-var apikey = process.env.SUBSCRIPTION_KEY;
+var request = require('request')
+var endpoint = 'https://westus.api.cognitive.microsoft.com/text/weblm/v1.0/calculateJointProbability?model=body'
+var apikey = process.env.SUBSCRIPTION_KEY || ''
 
-var redis = require('redis');
-var cache = {};
+var redis = require('redis')
+var cache = {}
 
-if (!!process.env.REDIS_DISABLED) {
-  cache.get = function(key, callback) { callback(null, null) }
-  cache.set = function(key, value) {}
+var debug = require('debug')('spellah')
+
+if (process.env.REDIS_DISABLED) {
+  cache.get = function (key, callback) { callback(null, null) }
+  cache.set = function (key, value) {}
 } else {
   cache = redis.createClient(process.env.REDIS_PORT || 6379, process.env.REDIS_HOST)
-  if (!!process.env.REDIS_KEY) cache.auth(process.env.REDIS_KEY)
-
-  cache.on('error', function (er) {
-    console.error(er.stack)
-  })
+  if (process.env.REDIS_KEY) cache.auth(process.env.REDIS_KEY)
 }
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Spellah' });
-});
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Spellah' })
+})
 
-function eachCons(a, size, callback) {
-  if (a.length > size)
-    for (var j = 0; j <= (a.length - size); j++) callback(a.slice(j, j + size))
-  else
-    callback(a)
+function eachCons (a, size, callback) {
+  if (a.length > size) { for (var j = 0; j <= (a.length - size); j++) callback(a.slice(j, j + size)) } else { callback(a) }
 }
 
-function mapOf(value) {
+function mapOf (value) {
   var map = {}
-  value.results.forEach(function(pair) { map[pair.words] = pair.probability })
+  if (value.results === undefined) return map
+  value.results.forEach(function (pair) { map[pair.words] = pair.probability })
   return map
 }
 
-function check(tokens, ngrams) {
+function check (tokens, ngrams) {
   var levels = new Array(tokens.length)
 
   // initialization & unigram check
@@ -48,7 +44,7 @@ function check(tokens, ngrams) {
   // bigram check
   if (levels.length > 1) {
     for (var i = 0, j = 1, len = levels.length; j < len; i++, j++) {
-      var token1 = tokens[i], token2 = tokens[j]
+      var token1 = tokens[i]; var token2 = tokens[j]
       var joint = [token1, token2].join(' ')
       if (ngrams[token1] + ngrams[token2] > ngrams[joint]) {
         levels[i] += (ngrams[token1] < ngrams[token2]) ? 2 : 1
@@ -60,43 +56,43 @@ function check(tokens, ngrams) {
   return levels
 }
 
-function reply(req, res, tokens, ngrams) {
+function reply (req, res, tokens, ngrams) {
   res.format({
-    json: function() {
-      res.send({ id: req.query.id, check: check(tokens, ngrams), ngrams: ngrams });
+    json: function () {
+      res.send({ id: req.query.id, check: check(tokens, ngrams), ngrams: ngrams })
     }
-  });
+  })
 }
 
-router.get('/check', function(req, res) {
+router.get('/check', function (req, res) {
   var key = 'weblm|' + req.query.q
 
   var tokens = req.query.q.split(' ')
   var body = []
 
   for (var i = tokens.length; i > 0; i--) {
-    eachCons(tokens, i, function(slice) { body.push(slice.join(' ')) })
+    eachCons(tokens, i, function (slice) { body.push(slice.join(' ')) })
   }
 
   cache.get(key, function (err, cached) {
     if (err || !cached) {
-      console.log('cache miss "' + req.query.q + '"')
+      debug('cache miss "' + req.query.q + '"')
       var headers = {
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': apikey
       }
-      var queries = JSON.stringify({queries: body})
+      var queries = JSON.stringify({ queries: body })
       request.post({ headers: headers, qs: 'model=body', url: endpoint, body: queries }, function (e, r, value) {
         var ngrams = mapOf(JSON.parse(value))
         cache.set(key, value)
         reply(req, res, tokens, ngrams)
       })
     } else {
-      console.log('cache hit "' + req.query.q + '" => ' + cached)
+      debug('cache hit "' + req.query.q + '" => ' + cached)
       var ngrams = mapOf(JSON.parse(cached))
       reply(req, res, tokens, ngrams)
     }
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
